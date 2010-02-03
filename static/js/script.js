2fcf86bf6c2e31e2da793b4afbcce779482fa17b
Bernhard Posselt <bernhard.posselt@gmx.at>
2010-02-03 15:05:35 +0100

    added gplv3 licences

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,25 @@
 /**
+ * Laudio - A webbased musicplayer
+ *
+ * Copyright (C) 2010 Bernhard Posselt, bernhard.posselt@gmx.at
+ *
+ * Laudio is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Laudio is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+/**
  * Function to change a song and update the now playing header
  *
  * @param String id:    The id of the tablerow e.g. row1
