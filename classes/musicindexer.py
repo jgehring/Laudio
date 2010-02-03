4e583f8190c116eb3df425bb359af4d5b632b6ba
Bernhard Posselt <bernhard.posselt@gmx.at>
2010-02-03 14:59:36 +0100

    added gplv3 licences

--- classes/musicindexer.py
+++ classes/musicindexer.py
@@ -1,8 +1,24 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
-
 """
-    An indexer which searches for existing music
+Laudio - A webbased musicplayer
+
+Copyright (C) 2010 Bernhard Posselt, bernhard.posselt@gmx.at
+
+Laudio is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+Laudio is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
 """
 
 import os
@@ -11,7 +27,9 @@ from laudio.models import Song
 import mutagen
 import time
 
-
+"""
+    An indexer which searches for existing music
+"""
 class MusicIndexer (object):
     
     def __init__(self, musicDir):
