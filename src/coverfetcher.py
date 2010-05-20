fcf995ef67c1aab7b68beb0f76e359baace183db
Bernhard Posselt <bernhard.posselt@gmx.at>
2010-05-21 01:51:39 +0200

    improved xml scanning with xpath

--- src/coverfetcher.py
+++ src/coverfetcher.py
@@ -96,8 +96,13 @@ class CoverFetcher(object):
         
         try:
             response = urllib2.urlopen(full_url)
+            # TODO: maybe we could reduce code size to one line with 
+            #       xpath method when parsing the xml
             elements = etree.fromstring(response.read())
-            return elements.xpath('/lfm/album/image[@size="extralarge"]/text()')[0]
+            if elements.get("status") == "ok":
+                return elements.xpath('/lfm/album/image[@size="extralarge"]/text()')[0]
+            else:
+                return None;
 
         except (URLError, HTTPError, UnicodeEncodeError):
             return None
