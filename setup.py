ac5452d8c195c4796b262fd2aaa132aba1fb11e6
Bernhard Posselt <bernhard.posselt@gmx.at>
2011-02-23 22:22:04 +0100

    pre 0.4.3 preperations

--- setup.py
+++ setup.py
@@ -3,10 +3,14 @@
 from distutils.core import setup
 
 setup(name='laudio',
-	version='0.4.2.2',
+	version='0.4.3',
 	description='laudio Music Player',
 	author='Bernhard Posselt',
 	author_email='bernhard.posselt@gmx.at',
 	url='https://github.com/Raydiation/Laudio',
 	packages=['laudio', 'laudio/src', 'laudio/src/song', 'laudio/src/song/formats'],
+	package_data={'' : ['laudio/media/*', 'laudio/media/js/*', 'laudio/media/style/*', 
+'laudio/media/style/img/*', 'laudio/tpl/*', 'laudio/tpl/inc/*', 'laudio/tpl/requests/*', 
+'laudio/tpl/settings/*', 'laudio/src/*', 'laudio/src/javascript/func/*', 
+'laudio/src/javascript/inc/*', 'laudio/src/javascript/ui/*']}
 )
