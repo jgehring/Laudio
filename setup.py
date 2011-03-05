873786b2447d1544bcc55c6734a0817b9ceaff83
Bernhard Posselt <bernhard.posselt@gmx.at>
2011-03-05 03:41:42 +0100

    pre 0.5.1.1 preparations

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='laudio',
-	version='0.5.1.0',
+	version='0.5.1.1',
 	description='laudio Music Player',
 	author='Bernhard Posselt',
 	author_email='bernhard.posselt@gmx.at',
